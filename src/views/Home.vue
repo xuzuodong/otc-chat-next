9269d21081fcb8212a31dc2c0b2e81db8c1b8d9a
许作栋 <xzd@33.cn>
2021-03-29 10:24:56 +0800

    setup project

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="home">
+        <h2>This is a Vue 3 component!</h2>
+        <button @click="increase">Clicked {{ count }} times.</button>
+    </div>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+export default defineComponent({
+    setup() {
+        const count = ref(0)
+        const increase = () => {
+            count.value++
+        }
+
+        return {
+            count,
+            increase,
+        }
+    },
+})
+</script>
