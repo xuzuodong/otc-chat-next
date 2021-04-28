9cab13ee856e4c394dbeabedab1b0a7f2afaec1d
许作栋 <xzd333333@163.com>
2021-04-28 15:32:17 +0800

    fix: 兼容部分安卓无法调用 Blob.prototype.arrayBuffer

--- src/utils/fzm-message-protocol/decodeMessage.ts
+++ src/utils/fzm-message-protocol/decodeMessage.ts
@@ -52,7 +52,7 @@ const decode = (data: Uint8Array): { header: Header; body: Uint8Array } => {
     if (bodyData.length) {
         body = bodyData
     } else {
-        body = new Uint8Array
+        body = new Uint8Array()
     }
 
     return {
@@ -69,3 +69,21 @@ export default (rawMsg: Blob): Promise<{ header: Header; body: Uint8Array }> => 
         })
     })
 }
+
+// 兼容部分安卓无法调用 Blob.prototype.arrayBuffer
+// https://gist.github.com/hanayashiki/8dac237671343e7f0b15de617b0051bd
+;(function () {
+    File.prototype.arrayBuffer = File.prototype.arrayBuffer || myArrayBuffer
+    Blob.prototype.arrayBuffer = Blob.prototype.arrayBuffer || myArrayBuffer
+
+    function myArrayBuffer(this: Blob) {
+        // this: File or Blob
+        return new Promise((resolve) => {
+            const fr = new FileReader()
+            fr.onload = () => {
+                resolve(fr.result)
+            }
+            fr.readAsArrayBuffer(this)
+        })
+    }
+})()
